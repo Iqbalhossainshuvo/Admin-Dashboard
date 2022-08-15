e70c0b8c628052930628f1caae383c6764afefc6
Iqbalhossainshuvo <mdiqbalhossain203050@gmail.com>
2022-08-15 18:23:27 +0600

    Admin Dashbord complite

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+    {
+        productName: 'Iphone',
+        productNumber: '85931',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Waltone',
+        productNumber: '85842',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Redmi',
+        productNumber: '89548',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Hp 840 G3',
+        productNumber: '58648',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Iphone 13 pro max',
+        productNumber: '85931',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Camera 4K Drone',
+        productNumber: '58468',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: ' Hp Laptop',
+        productNumber: '25484',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+
+];
